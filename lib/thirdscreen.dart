22d5ccd37a4f313c6700e3efac23b31f5bd8913b
Paribeshlamsal <161189337+Paribeshlamsal@users.noreply.github.com>
2025-04-24 14:22:50 +0545

    Updates

---
+++ lib/thirdscreen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Third Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Welcome to the Third Screen!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Go Back'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
